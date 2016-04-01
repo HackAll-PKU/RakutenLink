b7867c9b6387a3cc48787340dfc45b8c01e84034
archimekai <bliewhale@163.com>
2016-04-01 23:10:04 +0800

    Controller对View操作的接口基本完成

--- src/View/RakutenLinkBlock.java
+++ src/View/RakutenLinkBlock.java
@@ -7,7 +7,7 @@ import javax.swing.*;
  * 连连看中每一个方块的类型。继承自Jlabel <br>
  * 增加了row和column来记录该方块的位置，暂定以左上角的方块为坐标0，0, 以此为基准，向下为row增加的方向，向右为column增加的方向
  */
-public class RakutenLinkLable extends JLabel {
+public class RakutenLinkBlock extends JLabel {
     public int row;
     public int column;
 }
--- src/View/ViewUpdatable.java
+++ src/View/ViewUpdatable.java
@@ -8,7 +8,24 @@ package View;
 public interface ViewUpdatable {
 
     /**
-     * 当两个方块被成功消去时调用,需要被消去的两个方块的横纵坐标,用于绘制消去成功时的有关动画和声音
+     * 使特定方块处于选中状态，需要知道选中方块的横纵坐标。
+     *
+     * @param rowBlock 选中方块的横坐标
+     * @param columnBlock 选中方块的纵坐标
+     */
+    public void makeBlockSelected(int rowBlock, int columnBlock);
+
+    /**
+     * 使特定方块从被选中状态转变为未被选中状态，需要知道被操作方块的横纵坐标
+     *
+     * @param rowBlock
+     * @param columnBlock
+     */
+    public void makeBlockUnselected(int rowBlock, int columnBlock);
+
+
+    /**
+     * 当两个方块被成功消去时调用,需要被消去的两个方块的横纵坐标,用于绘制消去成功时的有关动画和声音, 并且会将有关的方块设置为不可见
      *
      * @param rowBlock1
      * @param columnBlock1
@@ -29,13 +46,36 @@ public interface ViewUpdatable {
     public void didClearTwoBlocksUnsuccessful(int rowBlock1, int columnBlock1, int rowBlock2, int columnBlock2);
 
     /**
+     * 通知view游戏进入了死局。
+     */
+    public void noValidFutureActions();
+
+    /**
+     * 通知view游戏剩余时间已耗尽。
+     */
+    public void noTimeRemaining();
+
+    /**
+     * 成功完成游戏。
+     */
+    public void didSuccess();
+
+    /**
      * 重置界面。用于游戏启动时对界面的初始化以及用户重新开始游戏的操作。
      */
     public void reset();
 
+    /**
+     * 初始化界面。
+     */
+    public void initialize();
 
+    /**
+     * 强制将model的数据同步进入view中，可以解决游戏中（可能出现的）显示和数据不同步的问题。<br>
+     * 供debug使用。
+     */
+    public void syncDataFromModel();
 
-    // TODO 用户点击的事件  是由view自动更新还是先由view交给controller，再由controller调用view更新？
     // TODO 进度条的时间由谁来维护？
 
 }
