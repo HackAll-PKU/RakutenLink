a6c488349cfe91654f5e7234af1d2b1896585124
Zac Chan <sunshinecltzac@gmail.com>
2016-04-02 11:09:40 +0800

    更新RakutenLinkBlock类

--- src/View/RakutenLinkBlock.java
+++ src/View/RakutenLinkBlock.java
@@ -8,6 +8,12 @@ import javax.swing.*;
  * 增加了row和column来记录该方块的位置，暂定以左上角的方块为坐标0，0, 以此为基准，向下为row增加的方向，向右为column增加的方向
  */
 public class RakutenLinkBlock extends JLabel {
-    public int row;
-    public int column;
+
+    public final int row;
+    public final int column;
+
+    public RakutenLinkBlock(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 }
